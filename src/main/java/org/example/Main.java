8a2adb8fb5758e1c77ac1c827ed3283355577ceb
kirbiCAT <kirbicat2019@gmail.com>
2026-03-25 16:06:30 +0200

    println

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,10 +9,12 @@ public class Main {
 
             public static void main (String[]args) throws InterruptedException, IOException {
             //String processName = "Discord.exe";
+                System.out.println("log");
                 while(true){
                     ProcessMonitor.Checker();
                     //amend 1.1
                     //experiment
+
                 }
             }
         }
